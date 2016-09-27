7798b63a10befd28f24cdaf3c1420b1011baff2e
Mattia <guzzi.mattia@gmail.com>
2016-09-27 15:21:07 +0200

    a commit

---
+++ fonts/webfontloader.js
@@ -0,0 +1,13 @@
+/**
+ * Created by RiMaStInO on 26/09/2016.
+ */
+
+WebFontConfig = {
+    typekit: { id: 'xcp6dvx' }
+};
+
+(function(d) {
+    var wf = d.createElement('script'), s = d.scripts[0];
+    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js';
+    s.parentNode.insertBefore(wf, s);
+})(document);
